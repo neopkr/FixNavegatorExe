73ac632bf37b3c2d3205a8d1e53a297837e7c64f
neopkr <56449637+neopkr@users.noreply.github.com>
2021-02-04 15:51:58 -0300

    Initial Commit - Exe Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿
+namespace FixNavegator
+{
+    partial class Formd
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(40, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F);
+            this.label1.Location = new System.Drawing.Point(123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(556, 97);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FixNavegator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label2.Location = new System.Drawing.Point(32, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Google Chrome Fix";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(351, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label3.Location = new System.Drawing.Point(360, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Firefox Fix";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button3.Location = new System.Drawing.Point(40, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Cache Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button4.Location = new System.Drawing.Point(40, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button5.Location = new System.Drawing.Point(351, 197);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Open";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button6.Location = new System.Drawing.Point(489, 302);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Create New Profile";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Control;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button7.Location = new System.Drawing.Point(351, 255);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "SafeMode";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Control;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button8.Location = new System.Drawing.Point(489, 273);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Open Profile";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(431, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 39);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "→";
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Control;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button9.Location = new System.Drawing.Point(351, 284);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Profiles";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.Control;
+            this.button10.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button10.Location = new System.Drawing.Point(295, 109);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(208, 26);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Close all Navegators";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.Control;
+            this.button11.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button11.Location = new System.Drawing.Point(713, 415);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "Open Egde";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.Control;
+            this.button12.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button12.Location = new System.Drawing.Point(40, 255);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "Safe/Incog";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label5.Location = new System.Drawing.Point(12, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Pre-Alpha Build v0.1";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.linkLabel1.Location = new System.Drawing.Point(121, 428);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(35, 13);
+            this.linkLabel1.TabIndex = 17;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "About";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.SystemColors.Control;
+            this.linkLabel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.linkLabel2.Location = new System.Drawing.Point(162, 428);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(29, 13);
+            this.linkLabel2.TabIndex = 18;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Help";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.BackColor = System.Drawing.SystemColors.Control;
+            this.linkLabel3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.linkLabel3.Location = new System.Drawing.Point(197, 428);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(32, 13);
+            this.linkLabel3.TabIndex = 19;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Page";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Formd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.HelpButton = true;
+            this.Name = "Formd";
+            this.Text = "FixNavegator";
+            this.Load += new System.EventHandler(this.Formd_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace FixNavegator
+{
+    public partial class Formd : Form
+    {
+        string Title = "FixNavegator";
+        public Formd()
+        {
+            InitializeComponent();
+        }
+
+        private void Formd_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExecuteCommand("taskkill /f /im chrome.exe");
+            Thread.Sleep(2000);
+            Process.Start("chrome");
+            MessageBox.Show("Reinicio completado con éxito", Title);
+        }
+
+        public void ExecuteCommand(string _Command)
+        {
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + _Command);
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = false;
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            Console.WriteLine(result);
+        }
+        public void KillTaskByName(string taskName)
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName(taskName))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExecuteCommand("taskkill /f /im firefox.exe");
+            Thread.Sleep(2000);
+            Process.Start("firefox");
+            MessageBox.Show("Reinicio completado con éxito", Title);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("We are working in this", Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            /*
+            const string message = "Are you sure want delete all cache of Chrome?";
+            const string caption = "Cache Deleting";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+            {
+                MessageBox.Show("Deleting", caption);
+                List<string> strDirectories = Directory.GetDirectories(@"C:\Users\neopk\AppData\Local\Google\Chrome\User Data\Default\Cache", "*.*", SearchOption.AllDirectories).ToList();
+
+                foreach (string fichero in strDirectories)
+                {
+                    File.Delete(fichero);
+                }
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("Cancelled", caption);
+                
+            }
+            */
+        }
+            
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("chrome", "www.google.com");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Process.Start("firefox", "www.google.com");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FirefoxProfilesIssues();
+        }
+
+        public void FirefoxProfilesIssues()
+        {
+            object myValue;
+            string message, title, defaultValue;
+            message = "Username: ";
+            title = "FixNavegator";
+            defaultValue = "Insert a User Profile Name";
+            myValue = Interaction.InputBox(message, title, defaultValue);
+            if((string)myValue == "")
+            {
+                myValue = defaultValue;
+                Interaction.MsgBox("MyValue = " + myValue.ToString(),
+                    MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Enter a UserProfile Name!");
+            }
+            else
+            {
+                Interaction.MsgBox("UserProfile Created Succefully" + Environment.NewLine + " New UserProfile = " + myValue.ToString() + "!", MsgBoxStyle.OkOnly | MsgBoxStyle.Information);
+            }
+            string _PROFILENAME = myValue.ToString();
+            Process.Start("firefox", "-CreateProfile " + _PROFILENAME);
+            Process.Start("firefox", "-p");
+        }
+
+        public void FirefoxSafeMode()
+        {
+            string message, title;
+            message = "Opening Firefox in Safe Mode";
+            title = "FixNavegator";
+            MessageBox.Show(message, title);
+            Process.Start("firefox", "-safe-mode -console");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FirefoxSafeMode();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            object myValue;
+            string message, title, defaultValue;
+            message = "Profile Name: " + Environment.NewLine + "Note: Exist 2 default profile: " + Environment.NewLine + "1.- default-release" + Environment.NewLine + "2.- default";
+            title = "FixNavegator";
+            defaultValue = "Insert a User Profile Name";
+            myValue = Interaction.InputBox(message, title, defaultValue);
+            if ((string)myValue == "")
+            {
+                myValue = defaultValue;
+                Interaction.MsgBox("MyValue = " + myValue.ToString(),
+                    MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Enter a UserProfile Name!");
+            }
+            else
+            {
+                Interaction.MsgBox("UserProfile Selected: " + myValue.ToString() + "!", MsgBoxStyle.OkOnly | MsgBoxStyle.Information);
+            }
+            string _PROFILENAME = myValue.ToString();
+            char comilla = '"';
+            Process.Start("firefox", "-p " + comilla + _PROFILENAME + comilla);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Process.Start("firefox", "-p");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            KillTaskByName("chrome");
+            KillTaskByName("firefox");
+            KillTaskByName("opera");
+            KillTaskByName("msedge");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Process.Start("msedge");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Process.Start("chrome", "/incognito");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://fixnavegator.herokuapp.com/");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+        }
+
+        public void Picture()
+        {
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.youtube.com/watch?v=egYTYpleY0U&ab_channel=Macsi");
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void button13_Click_1(object sender, EventArgs e)
+        {
+        }
+    }
+}
